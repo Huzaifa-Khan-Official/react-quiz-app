5b76a86339a97dde4d79c2911361c7611425a7cb
Huzaifa Khan <m.huzaifakhan2004@gmail.com>
2024-01-06 21:32:58 +0500

    restart issue solved

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import animatedLogo from "./animatedlogo.gif";
 function App() {
   let [questionList, setQuestionList] = useState([]);
   let [currIndex, setCurrIndex] = useState(0);
+  // const [flag, setFlag] = useState(false);
 
   useEffect(() => {
     getDataFromAPI()
@@ -19,14 +20,21 @@ function App() {
   }
 
   if (!questionList.length) {
-    return <img src={animatedLogo} alt="" className='animatedLoader'/>
+    return <img src={animatedLogo} alt="" className='animatedLoader' />
   }
-
+  
+  const restart = () => {
+    setCurrIndex(0)
+  }
 
   return (
     <div className="App">
-      <h4>{questionList[0].question.text}</h4>
-      <button onClick={setCurrIndex(oldValue => oldValue + 1)}>Next</button>
+      <h4>{questionList[currIndex].question.text}</h4>
+      <button onClick={() => setCurrIndex(currIndex => currIndex + 1)} style={{display: currIndex ==9 ? "none" : "block"}}>
+        Next
+      </button>
+
+      <button style={{display: currIndex == 9 ? "block" : "none"}} onClick={restart}>Restart</button>
     </div>
   );
 }
