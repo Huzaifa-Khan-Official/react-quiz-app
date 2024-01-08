200155cdf21af27c6b018f47668799d8e5e30bc1
Huzaifa Khan <m.huzaifakhan2004@gmail.com>
2024-01-08 21:24:27 +0500

    trying again

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,62 @@
+// import { useEffect, useState } from 'react';
+// import './App.css';
+// import animatedLogo from "./animatedlogo.gif";
+// import Options from './Components/Options/Options';
+
+// function App() {
+//   let [questionList, setQuestionList] = useState([]);
+//   let [currIndex, setCurrIndex] = useState(0);
+//   const [shuffledArray, setShuffledArray] = useState([]);
+
+//   useEffect(() => {
+//     getDataFromAPI()
+//   }, [])
+
+//   function getDataFromAPI() {
+//     fetch('https://the-trivia-api.com/v2/questions')
+//       .then(res => res.json())
+//       .then(res => {
+//         setQuestionList(res);
+//       })
+//   }
+
+//   if (!questionList.length) {
+//     return <img src={animatedLogo} alt="" className='animatedLoader' />
+//   }
+
+//   const restart = () => {
+//     setCurrIndex(0)
+//   }
+
+//   let optionsArr = questionList[currIndex].incorrectAnswers;
+//   let correctAns = questionList[currIndex].correctAnswer;
+//   optionsArr.push(correctAns);
+
+//   // const nextQuestion = () => {
+
+//   // }
+
+//   return (
+//     <div className="App">
+//       <h4>{questionList[currIndex].question.text}</h4>
+//       <Options optionsArr={optionsArr} correctAns={correctAns}/>
+//       <button onClick={() => setCurrIndex(currIndex => currIndex + 1)} style={{ display: currIndex == 9 ? "none" : "block" }}>
+//         Next
+//       </button>
+
+//       <button style={{ display: currIndex == 9 ? "block" : "none" }} onClick={restart}>Restart</button>
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import { useEffect, useState } from 'react';
 import './App.css';
-import animatedLogo from "./animatedlogo.gif";
-import Options from './Components/Options/Options';
+import animatedLogo from "./animatedlogo.gif"; 
 
-function App() {
-  let [questionList, setQuestionList] = useState([]);
-  let [currIndex, setCurrIndex] = useState(0);
-  const [shuffledArray, setShuffledArray] = useState([]);
-
-  useEffect(() => {
-    getDataFromAPI()
-  }, [])
-
-  function getDataFromAPI() {
-    fetch('https://the-trivia-api.com/v2/questions')
-      .then(res => res.json())
-      .then(res => {
-        setQuestionList(res);
-      })
-  }
-
-  if (!questionList.length) {
-    return <img src={animatedLogo} alt="" className='animatedLoader' />
-  }
-
-  const restart = () => {
-    setCurrIndex(0)
-  }
-
-  let optionsArr = questionList[currIndex].incorrectAnswers;
-  let correctAns = questionList[currIndex].correctAnswer;
-  optionsArr.push(correctAns);
-  return (
-    <div className="App">
-      <h4>{questionList[currIndex].question.text}</h4>
-      <Options optionsArr={optionsArr} correctAns={correctAns}/>
-      <button onClick={() => setCurrIndex(currIndex => currIndex + 1)} style={{ display: currIndex == 9 ? "none" : "block" }}>
-        Next
-      </button>
-
-      <button style={{ display: currIndex == 9 ? "block" : "none" }} onClick={restart}>Restart</button>
-    </div>
-  );
+function App () {
+  
 }
 
 export default App;
--- src/Components/Options/Options.jsx
+++ src/Components/Options/Options.jsx
@@ -1,28 +1,34 @@
-import React, { useState } from 'react'
-import "./Options.css"
+// import React, { useState } from 'react'
+// import "./Options.css"
 
-export default function Options({ optionsArr, correctAns }) {
-    let [checkInput, setCheckInput] = useState(false);
-    optionsArr.sort(() => Math.random() - 0.5);
-    const selectedOption = (selectedOption) => {
-        if (correctAns == selectedOption) {
-            console.log("sahi kya hai");
-        }
-        console.log(selectedOption);
-    }
-    return (
-        <div className='OptionsDiv'>
-            {/* {optionsArr.map((value, index) => <button key={index} onClick={() => selectedOption(value)}>{value}</button>)} */}
-            {optionsArr.map((value, index) =>
-                <div className="optDiv" key={index}>
-                    <input type="radio" name="option" />
-                    <label value={index}>{value}</label>
-                </div>
-            )}
-            {/* <div className="optDiv">
-                <input checked={checkInput} type="radio" name='option'/>
-                <label onClick={() => setCheckInput(!checkInput)}>Ok</label>
-            </div> */}
-        </div>
-    )
-}
+// export default function Options({ optionsArr, correctAns }) {
+//     // let [checkInput, setCheckInput] = useState(false);
+//     let [selOptIndex, setselOptIndex] = useState("");
+//     optionsArr.sort(() => Math.random() - 0.5);
+//     const selectedOption = (selectedOption) => {
+//         if (correctAns == selectedOption) {
+//             console.log("sahi kya hai");
+//         }
+//         console.log(selectedOption);
+//     }
+
+//     const checkedLabel = (index) => {
+//         setselOptIndex(index);
+//         console.log(selOptIndex);
+//     }
+//     return (
+//         <div className='OptionsDiv'>
+//             {/* {optionsArr.map((value, index) => <button key={index} onClick={() => selectedOption(value)}>{value}</button>)} */}
+//             {optionsArr.map((value, index) =>
+//                 <div className="optDiv" key={index}>
+//                     <input type="radio" name="option" value={value}/>
+//                     <label value={index} onClick={() => checkedLabel(index)}>{value}</label>
+//                 </div>
+//             )}
+//             {/* <div className="optDiv">
+//                 <input checked={checkInput} type="radio" name='option'/>
+//                 <label onClick={() => setCheckInput(!checkInput)}>Ok</label>
+//             </div> */}
+//         </div>
+//     )
+// }
