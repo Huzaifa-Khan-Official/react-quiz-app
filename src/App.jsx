ad35e7186f576a5139670ba8948be89f26e330e2
Huzaifa Khan <m.huzaifakhan2004@gmail.com>
2024-01-08 23:06:48 +0500

    UI done

--- src/App.jsx
+++ src/App.jsx
@@ -53,10 +53,68 @@
 
 import { useEffect, useState } from 'react';
 import './App.css';
-import animatedLogo from "./animatedlogo.gif"; 
+import animatedLogo from "./animatedlogo.gif";
 
-function App () {
-  
+function App() {
+  let [currQuestion, setCurrQuestion] = useState(0);
+  let [questionsArray, setQuestionsArray] = useState([]);
+  let [clickedOpt, setClickedOpt] = useState(0);
+
+  function getDataFromAPI() {
+    fetch('https://the-trivia-api.com/v2/questions')
+      .then(res => res.json())
+      .then(res => {
+        setQuestionsArray(res);
+      })
+  }
+
+  useEffect(() => {
+    getDataFromAPI()
+  }, []);
+
+  if (!questionsArray.length) {
+    return <img src={animatedLogo} alt="" className='animatedLoader' />
+  }
+
+  let optionsArr = questionsArray[currQuestion].incorrectAnswers;
+  let correctAns = questionsArray[currQuestion].correctAnswer;
+  optionsArr.push(correctAns);
+
+  const nextQuestion = () => {
+    if (currQuestion < questionsArray.length - 1) {
+      setCurrQuestion(currQuestion + 1);
+    }
+  }
+
+
+  return (
+    <>
+      <h1>Quiz APP</h1>
+      <div className="container">
+        <div className="questionDiv">
+          <h3>{currQuestion + 1}.{questionsArray[currQuestion].question.text}</h3>
+        </div>
+        <div className="optionsDiv">
+          {optionsArr.map((option, i) => {
+            return (
+              <button
+                key={i}
+                className={`option-btn ${clickedOpt == i + 1 ? "checked" : null
+                  }`}
+                onClick={() => setClickedOpt(i + 1)}
+              >
+                {option}
+              </button>
+            )
+          })}
+        </div>
+
+        <button id="next-button" onClick={nextQuestion}>
+          Next
+        </button>
+      </div>
+    </>
+  );
 }
 
 export default App;
