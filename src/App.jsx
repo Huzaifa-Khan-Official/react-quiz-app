8acb0560f46f921d1d4fa6b85240ef136c33e773
Huzaifa Khan <m.huzaifakhan2004@gmail.com>
2024-01-15 23:20:45 +0500

    shuffle issue solved

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import './App.css';
 import animatedLogo from "./animatedlogo.gif";
 
 function App() {
-  let [currQuestion, setCurrQuestion] = useState(0);
-  let [questionsArray, setQuestionsArray] = useState([]);
-  let [clickedOpt, setClickedOpt] = useState(0);
-  let [isClicked, setIsClicked] = useState(false);
-  let [score, setScore] = useState(0);
+  const [currQuestion, setCurrQuestion] = useState(0);
+  const [questionsArray, setQuestionsArray] = useState([]);
+  const [clickedOpt, setClickedOpt] = useState(0);
+  const [isClicked, setIsClicked] = useState(false);
+  const [score, setScore] = useState(0);
   const [showResult, setShowResult] = useState(false);
-  let optionsArr = [];
+  const [optionsArr, setOptionsArr] = useState([]);
 
   function getDataFromAPI() {
     fetch('https://the-trivia-api.com/v2/questions')
@@ -20,17 +20,29 @@ function App() {
       })
   }
 
-  useEffect(() => {
-    getDataFromAPI();
-    optionsArr.sort(() => Math.random() - 0.5)
-  }, []);
+  const shuffleOptions = () => {
+    const options = [];
+    const incorrectOpts = questionsArray[currQuestion].incorrectAnswers;
+    options.push(...incorrectOpts);
+    const correctAns = questionsArray[currQuestion].correctAnswer;
+    options.push(correctAns);
 
-  if (!questionsArray.length) {
-    return <img src={animatedLogo} alt="" className='animatedLoader' />
+    options.sort(() => Math.random() - 0.5);
+    setOptionsArr(options);
   }
 
+  useEffect(() => {
+    getDataFromAPI();
+  }, []);
+
+  useEffect(() => {
+    if (questionsArray.length > 0) {
+      shuffleOptions();
+    }
+  }, [currQuestion, questionsArray]);
+
   const updateScore = () => {
-    if (optionsArr[clickedOpt] === correctAns) {
+    if (optionsArr[clickedOpt - 1] === questionsArray[currQuestion].correctAnswer) {
       setScore(score + 1);
     }
   }
@@ -40,27 +52,22 @@ function App() {
     setIsClicked(false);
     if (currQuestion < questionsArray.length - 1) {
       setCurrQuestion(currQuestion + 1);
-      setClickedOpt(null);
+      setClickedOpt(0);
     } else {
-      setShowResult(true)
+      setShowResult(true);
     }
   }
 
-  
-
-
-  let incorrectOpts = questionsArray[currQuestion].incorrectAnswers;
-  optionsArr.push(...incorrectOpts);
-  let correctAns = questionsArray[currQuestion].correctAnswer;
-  console.log(correctAns);
-  optionsArr.push(correctAns);
+  if (!questionsArray.length) {
+    return <img src={animatedLogo} alt="" className='animatedLoader' />
+  }
 
   return (
     <>
       <h1>Quiz APP</h1>
       <div className="container">
         {showResult ? (
-          < QuizResult score={score} totalScore={questionsArray.length} />
+          <QuizResult score={score} totalScore={questionsArray.length} />
         ) : (
           <>
             <div className="questionDiv">
@@ -71,19 +78,17 @@ function App() {
                 return (
                   <button
                     key={i}
-                    className={`option-btn ${clickedOpt === i + 1 ? "checked" : null
-                      }`}
+                    className={`option-btn ${clickedOpt === i + 1 ? "checked" : null}`}
                     onClick={() => { 
                       setClickedOpt(i + 1);
                       setIsClicked(true);
-                     }}
+                    }}
                   >
                     {option}
                   </button>
                 )
               })}
             </div>
-
             <button id="next-button" onClick={nextQuestion} disabled={!isClicked ? true : false}>
               Next
             </button>
@@ -94,4 +99,4 @@ function App() {
   );
 }
 
-export default App; 
+export default App;
